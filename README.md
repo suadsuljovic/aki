331f38d205ffb81da145391252a430b4855f4f47
Suad Suljovic <suad.suljovic92@gmail.com>
2024-09-02 13:41:10 +0200

    react projekat

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # aki
+
+## React
+
+React je biblioteka koja se najvise danas koristi za pravljenje web korisnickih interfejsa.
+Trenutno je jedan od najpopularnijih i moze se nauciti veoma brzo.
+
+https://react.dev/
+https://react.dev/learn/thinking-in-react
+https://react.dev/learn/start-a-new-react-project
+
+### Naraviti projekat upsomoc vitejs
+
+Odes na vite js i kopiras komandu za pravljenej novog projekat
+Pravimo projekat kojie se zove react-projkat i uz pomoc reacta i javascripte
+
+### Kako se pokrece projkat
--- prviCas/test.js
+++ prviCas/test.js
@@ -4,12 +4,12 @@
 // operators ✅
 // logical operators ✅
 // truthy i falsy ✅
-// conditionals
-// loops
-// objects
-// arrays
-// array methods
-// callbacks
+// conditionals ✅
+// loops ✅
+// objects ✅
+// arrays ✅
+// array methods ✅
+// callbacks/functions ✅
 // scope ✅
 
 // promises
@@ -77,9 +77,74 @@ if ("") {
 // && , ||
 
 // rezultat je prva falsy vrednost ili poslednja thruty
+// and operator je tacan ako su sve vrednosti tacne a netacan u svakom drugom slucaju
 console.log("sdfas" && false && "sdfs");
 console.log("sdfas" && 1324 && "sdfs");
 
 // rezulat je prva thruty vrednost ili poslenja falsy
+// or operator je tacan ako je bar jedna vrednost tacna a netacan u svakom drugom slucaju
 console.log(false || false || "sdfs");
 console.log("" || false || null);
+
+//conditionals ili uslovi
+
+if (true) {
+  console.log("izvrsilo se ");
+}
+
+if (false) {
+} else {
+}
+
+// loops ili ponavljanja
+
+for (let i = 0; i < 10; i++) {
+  console.log("ponovi");
+}
+
+//objects
+// propertije
+
+// deklaracija/pravljenje novog objekta
+var obj = { test: "suad" };
+
+console.log(obj.test);
+
+// dodavanje novog propertija
+obj.nova = "nesto novo";
+// brisanje nekog propertija sa objekta
+delete obj.nova;
+
+console.log(obj);
+
+// arrays ili nizovi
+
+var ni = [1, 2, 3];
+
+console.log(ni);
+
+// pristupanje nekoj vrednosti u nizu
+console.log(ni[0]);
+console.log(ni[2]);
+
+// dodavanje vrednosti na kraj niza
+ni.push(10);
+
+// ni.unShift()
+
+// za sklanjanje vrednosti ni.pop(), ni.shift()
+
+// array methoda postoje i map, forEach, reduce, filter, indexOf...
+
+// funkcije
+
+// deklaracija ili ti pravljenje
+function fn() {
+  console.log("izvrsi");
+}
+
+// pozivanje fukcija ; calling the function
+fn();
+
+// napisi funkciju koja racuna zbir dva broja
+// primer funkcije sum(5,2)
